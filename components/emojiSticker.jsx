d3ba16cce7e824dafd30a112d03f1a85b73cfd61
DiegoHrz <hernandezwdiego@gmail.com>
2024-04-28 23:38:05 -0500

    Add useSharedValue in emojiSticker

--- components/emojiSticker.jsx
+++ components/emojiSticker.jsx
@@ -1,7 +1,10 @@
 import { View, Image } from 'react-native'
-import Animated  from 'react-native-reanimated'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 const EmojiSticker = ({imageSize, stickerSource}) => {
+    const scaleImage = useSharedValue(imageSize);
+
   return (
     <View style={{top: -350}} >
         <Animated.Image source={stickerSource} style={{width: imageSize, height: imageSize}} resizeMode="contain" />
